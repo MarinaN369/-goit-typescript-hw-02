266162838808b7bfc4565cae1d5bf3cd7430b460
MarinaN369 <143936204+MarinaN369@users.noreply.github.com>
2024-04-27 08:04:10 +0300

    update

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -1,10 +1,12 @@
 import SearchBar from "../SearchBar/SearchBar";
 import css from "./App.module.css";
 
+
+
 function App() {
     return (
         <div className={css.container}>
-          <SearchBar  />
+          <SearchBar />
           
         </div>
       );
